cafcd8bd3f7191e0bb6b77b4941f4da19a99a3ff
maxoflondon <maxoflondon@gmail.com>
2023-08-04 02:57:30 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Test of pygame_gui with pygbag (linux)
+
+This is a test boilerplate setup for pygme_gui and python webassembly using pygbag.
+
+I found that browser hangs on `Setting [pygame_gui] up` and want to have starting point to work on the solution.
+
+To run the test `chmod +x run.sh` then `source run.sh .venv`
+
+Script will install virtualenv and dependencies then execute `main.py` without and with pygbag.
+
+To see debug from webassembly navigate to http://localhost:8000/#debug
---
+++ run.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# Usage:
+# chmod +x ./run.sh
+# source ./run.sh .venv
+#
+
+confirm() {
+    echo -e "\033[32m" 
+    read -rN1 -p "$* [Y/n]? "; [[ ${REPLY:0:1} = [Yy$'\n'] ]]; 
+    echo -e "\033[0m"
+}
+
+in_venv=$(python3 -c 'import sys; print ("1" if hasattr(sys, "real_prefix") else "0")')
+
+if [[ $in_venv ]]; then
+    deactivate
+fi
+
+python3 -m pip install --user virtualenv
+virtualenv -q -p /usr/bin/python3 $1
+source $1/bin/activate
+pip install -r requirements.txt
+
+confirm '[+] Run main.py without pygbag?'
+if [ -z $REPLY ]; then
+    python3 main.py
+fi
+
+confirm '[+] Run main.py with pygbag? (Y/n)'
+if [ -z $REPLY ]; then
+    pwd=${PWD##*/}
+    pygbag ../$pwd
+fi
